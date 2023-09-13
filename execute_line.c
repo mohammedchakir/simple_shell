5a545be4c87364deede6ed2f7ef05bea0472e964
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-09-13 19:10:00 +0100

    Update execute_line.c

--- execute_line.c
+++ execute_line.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * execute_line - gets commands and builtins
- * @datash: shell info
- * Return: 1.
+ * executeline - Processes commands and built-ins.
+ * @datash: Shell information.
+ * Return: 1 upon successful execution.
  */
-int execute_line(data_shell *datash)
+int executeline(data_shell *datash)
 {
 	int (*built_in)(data_shell *datash);
 
 	if (datash->args[0] == NULL)
 		return (1);
 
-	built_in = get_built_in(datash->args[0]);
+	built_in = get_builtin(datash->args[0]);
 
 	if (built_in != NULL)
 		return (built_in(datash));
 
-	return (exe_cmd(datash));
+	return (execmd(datash));
 }
