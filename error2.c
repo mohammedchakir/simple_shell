ccf06ccfd6c6e5825169abee78cb28bb706ef73f
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-09-14 00:42:15 +0100

    Update error2.c

--- error2.c
+++ error2.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * env_err - env error message
- * @datash: data from current shell
- * Return: Full error message
+ * env_err - Generates an error message for environment-related issues.
+ * @datash: Data from the current shell.
+ * Return: The complete error message.
  */
 char *env_err(data_shell *datash)
 {
 	int len;
 	char *err;
 	char *str_var;
-	char *message;
+	char *msg;
 
 	str_var = aux_itoa(datash->counter);
-	message = ": Unable to add/remove from environment\n";
+	msg = ": Unable to add/remove from environment\n";
 	len = _strlen(datash->av[0]) + _strlen(str_var);
-	len += _strlen(datash->args[0]) + _strlen(message) + 4;
+	len += _strlen(datash->args[0]) + _strlen(msge) + 4;
 	err = malloc(sizeof(char) * (len + 1));
 	if (err == 0)
 	{
@@ -28,15 +28,15 @@ char *env_err(data_shell *datash)
 	_strcat(err, str_var);
 	_strcat(err, ": ");
 	_strcat(err, datash->args[0]);
-	_strcat(err, message);
+	_strcat(err, msg);
 	_strcat(err, "\0");
 	free(str_var);
 	return (err);
 }
 /**
- * path_denied_err - path denied error message
- * @datash: data from current shell
- * Return: Full error message
+ * path_denied_err - Generates an error message for permission denied when accessing a path.
+ * @datash: Data from the current shell.
+ * Return: The complete error message.
  */
 char *path_denied_err(data_shell *datash)
 {
