66fccf0f1510e088c18e5d93f2318073fbcf44e3
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-09-14 15:18:25 +0100

    Update error2.c

--- error2.c
+++ error2.c
@@ -7,16 +7,16 @@
  */
 char *env_err(data_shell *datash)
 {
-	int len;
+	int lenght;
 	char *err;
 	char *str_var;
 	char *msg;
 
-	str_var = aux_itoa(datash->counter);
+	str_var = aux_itoa(datash->ctr);
 	msg = ": Unable to add/remove from environment\n";
-	len = _strlen(datash->av[0]) + _strlen(str_var);
-	len += _strlen(datash->args[0]) + _strlen(msge) + 4;
-	err = malloc(sizeof(char) * (len + 1));
+	lenght = _strlen(datash->av[0]) + _strlen(str_var);
+	lenght += _strlen(datash->args[0]) + _strlen(msge) + 4;
+	err = malloc(sizeof(char) * (lenght + 1));
 	if (err == 0)
 	{
 		free(err);
@@ -40,13 +40,13 @@ char *env_err(data_shell *datash)
  */
 char *path_denied_err(data_shell *datash)
 {
-	int len;
+	int lenght;
 	char *str_var, *err;
 
-	str_var = aux_itoa(datash->counter);
-	len = _strlen(datash->av[0]) + _strlen(str_var);
-	len += _strlen(datash->args[0]) + 24;
-	err = malloc(sizeof(char) * (len + 1));
+	str_var = aux_itoa(datash->ctr);
+	lenght = _strlen(datash->av[0]) + _strlen(str_var);
+	lenght += _strlen(datash->args[0]) + 24;
+	err = malloc(sizeof(char) * (lenght + 1));
 	if (err == 0)
 	{
 		free(err);
