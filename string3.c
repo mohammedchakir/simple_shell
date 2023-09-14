677c2d0a8a8672d3e3b431a476fead782cffcaa2
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-09-14 15:58:17 +0100

    Update string3.c

--- string3.c
+++ string3.c
@@ -7,18 +7,18 @@
  */
 void rev_string(char *s)
 {
-	int counter = 0;
+	int ctr = 0;
 	int n, m;
 	char *_str, tem;
 
-	while (counter >= 0)
+	while (ctr >= 0)
 	{
-		if (s[counter] == '\0')
+		if (s[ctr] == '\0')
 			break;
-		counter++;
+		ctr++;
 	}
 	_str = s;
-	for (n = 0; n < (counter - 1); n++)
+	for (n = 0; n < (ctr - 1); n++)
 	{
 		for (m = n + 1; m > 0; m--)
 		{
