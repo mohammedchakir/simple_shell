cc59170c6c702e40928e3206c66e99554140e386
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-15 20:59:09 +0100

    Update environs1.c

--- environs1.c
+++ environs1.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * comp_env_names - Compares environment variable name.
+ * comp_env_names - Compares environment variable nam.
  * @nenv: The name of an environment variable.
  * @name: The name to compare against.
  * Return: 0 if the names match, otherwise a non-zero value.
