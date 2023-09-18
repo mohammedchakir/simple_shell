f0154fd298b8d2072425879a9c57f08d7b677265
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-18 10:38:38 +0100

    Update Str.c

--- Str.c
+++ Str.c
@@ -4,19 +4,7 @@
 
 	
 	
-	free(count);
-_strcat(err, msg2);
-
-	_strcat(err, msg);
-
-	_strcat(err, msg3);
-
-	_strccc(err, "\0");
-
-	write(STDERR_FILENO, err, lenght);
-
-	free(err);
-
+	
 	free(count);
 _strcat(err, msg2);
 
