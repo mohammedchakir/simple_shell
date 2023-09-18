23e15563dec33c0c7dac48b3086f6d83caf2229e
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-18 10:00:42 +0100

    Update Str.c

--- Str.c
+++ Str.c
@@ -11,13 +11,7 @@ char *_strcat(char *dest, const char *src)
 	int n;
 	int m;
 
-	for (n = 0; dest[n] != '\0'; n++)
-		;
-	for (m = 0; src[m] != '\0'; m++)
-	{
-		dest[n] = src[m];
-		n++;
-	}
+	
 	dest[n] = '\0';
 	return (dest);
 }
