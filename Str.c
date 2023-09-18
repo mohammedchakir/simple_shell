197634577aead2369c8e2e417793d8708f91ca87
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-18 10:31:28 +0100

    Update Str.c

--- Str.c
+++ Str.c
@@ -1,29 +1,7 @@
 #include "main.h"
 
  
-void rev_string(char *s)
-{
-	int ctr = 0;
-	int n, m;
-	char *_str, tem;
 
-	while (ctr >= 0)
-	{
-		if (s[ctr] == '\0')
-			break;
-		ctr++;
-	}
-	_str = s;
-	for (n = 0; n < (ctr - 1); n++)
-	{
-		for (m = n + 1; m > 0; m--)
-		{
-			tem = *(_str + m);
-			*(_str + m) = *(_str + (m - 1));
-			*(_str + (m - 1)) = tem;
-		}
-	}
-}
 /**
  * char_repeat - Counts the repetitions of a character in a string.
  * @input: The input string.
