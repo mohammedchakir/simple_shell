d24b670c9f4d0b914efb439c8a27adf5dd0eb055
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-18 10:33:48 +0100

    Update Str.c

--- Str.c
+++ Str.c
@@ -2,15 +2,7 @@
 
 	
 
-	msg2 = ": Syntax error: \"";
-	msg3 = "\" unexpected\n";
-	count = aux_itoa(datash->counter);
-	lenght = _strlen(datash->av[0]) + _strlen(count);
-	lenght += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
-	err = malloc(sizeof(char) * (lenght + 1));
-	if (err == 0)
-	{
-		free(count);
+	
 		return;
 	}
 	_strcpy(err, datash->av[0]);
