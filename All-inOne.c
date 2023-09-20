b9cdfaaa8d4b6908a51125cd79ca1e484a70592e
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-09-20 21:53:04 +0100

    Update All-inOne.c

--- All-inOne.c
+++ All-inOne.c
@@ -3,38 +3,6 @@
 
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The input string.
- * Return: The integer value parsed from the string.
- */
-int _atoi(char *s)
-{
-	unsigned int ctr = 0, size = 0;
-	int final = 0, sign = 1, power = 1;
-	unsigned int n;
-
-	while (*(s + ctr) != '\0')
-	{
-		if (size > 0 && (*(s + ctr) < '0' || *(s + ctr) > '9'))
-			break;
-		if (*(s + ctr) == '-')
-			sign *= -1;
-		if ((*(s + ctr) >= '0') && (*(s + ctr) <= '9'))
-		{
-			if (size > 0)
-				power *= 10;
-			size++;
-		}
-		ctr++;
-	}
-	for (n = ctr - size; n < ctr; n++)
-	{
-		final = final + ((*(s + n) - 48) * power);
-		power /= 10;
-	}
-	return (final * sign);
-}
-/**
  * par_cd - Change to the parent directory.
  * @datash: Current Shell informations.
  * Return: None.
