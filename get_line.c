6ea5efeb3afdca51eed51bbc5668c8edc19c1d34
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-24 17:42:43 +0100

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * bring_line - assigns the line var for get_line
- * @lineptr: Buffer that store the input str
- * @buffer: str that is been called to line
- * @n: size of line
- * @j: size of buffer
+ * bring_line - function assigns the line var for get_line.
+ * @lineptr: the Buffer that store the input string.
+ * @buffer: the string that is been called to line.
+ * @n: the size of line.
+ * @m: the size of buffer.
  */
-void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t m)
 {
 
 	if (*lineptr == NULL)
 	{
 		if  (j > BUFSIZE)
-			*n = j;
+			*n = m;
 
 		else
 			*n = BUFSIZE;
 		*lineptr = buffer;
 	}
-	else if (*n < j)
+	else if (*n < m)
 	{
-		if (j > BUFSIZE)
-			*n = j;
+		if (m > BUFSIZE)
+			*n = m;
 		else
 			*n = BUFSIZE;
 		*lineptr = buffer;
@@ -34,15 +34,15 @@ void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 	}
 }
 /**
- * get_line - Read inpt from stream
- * @lineptr: buffer that stores the input
- * @n: size of lineptr
- * @stream: stream to read from
- * Return: The number of bytes
+ * get_line - function Read inpt from stream.
+ * @lineptr: the buffer that stores the input.
+ * @n: the size of lineptr.
+ * @stream: the stream to read from.
+ * Return: The number of bytes.
  */
 ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 {
-	int i;
+	int n;
 	static ssize_t input;
 	ssize_t retval;
 	char *buffer;
@@ -60,12 +60,12 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 	while (t != '\n')
 	{
 		i = read(STDIN_FILENO, &t, 1);
-		if (i == -1 || (i == 0 && input == 0))
+		if (n == -1 || (n == 0 && input == 0))
 		{
 			free(buffer);
 			return (-1);
 		}
-		if (i == 0 && input != 0)
+		if (n == 0 && input != 0)
 		{
 			input++;
 			break;
@@ -78,7 +78,7 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 	buffer[input] = '\0';
 	bring_line(lineptr, n, buffer, input);
 	retval = input;
-	if (i != 0)
+	if (n != 0)
 		input = 0;
 	return (retval);
 }
