beaaa812e51db2792aabcb283bc2fa7ae1d6d984
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-24 17:38:09 +0100

    Update get_error.c

--- get_error.c
+++ get_error.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * get_error - calls the error according the builtin, syntax or permission
- * @datash: data structure that contains arguments
- * @eval: error value
- * Return: error
+ * get_error - function calls error according the builtin, syntax or perm.
+ * @datash: the data structure that contains arguments.
+ * @eval: the error value.
+ * Return: the error.
  */
 int get_error(data_shell *datash, int eval)
 {
-	char *error;
+	char *err;
 
 	switch (eval)
 	{
 	case -1:
-		error = error_env(datash);
+		err = error_env(datash);
 		break;
 	case 126:
-		error = error_path_126(datash);
+		err = error_path_126(datash);
 		break;
 	case 127:
-		error = error_not_found(datash);
+		err = error_not_found(datash);
 		break;
 	case 2:
 		if (_strcmp("exit", datash->args[0]) == 0)
@@ -29,10 +29,10 @@ int get_error(data_shell *datash, int eval)
 		break;
 	}
 
-	if (error)
+	if (err)
 	{
-		write(STDERR_FILENO, error, _strlen(error));
-		free(error);
+		write(STDERR_FILENO, err, _strlen(err));
+		free(err);
 	}
 
 	datash->status = eval;
