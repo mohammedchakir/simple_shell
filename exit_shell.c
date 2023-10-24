38645bbee703165679558ca645b17ab7f6301474
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-24 17:32:18 +0100

    Update exit_shell.c

--- exit_shell.c
+++ exit_shell.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * exit_shell - exits the shell
- *
- * @datash: data relevant (status and args)
+ * exit_shell - function exits the shell.
+ * @datash: the data relevant.
  * Return: 0 on success.
  */
 int exit_shell(data_shell *datash)
