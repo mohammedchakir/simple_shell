5e0bf2f9574d92a45e40488dfaae48bb32ba1c07
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2023-10-24 15:51:28 +0100

    Update and rename aux_help2.c to _help2.c

--- _help2.c
+++ _help2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * aux_help - Help information for the builtin help.
- * Return: no return
+ * aux_help - the help information for the builtin help.
+ * Return: no return printed.
  */
 void aux_help(void)
 {
@@ -15,8 +15,8 @@ void aux_help(void)
 	write(STDOUT_FILENO, help, _strlen(help));
 }
 /**
- * aux_help_alias - Help information for the builtin alias.
- * Return: no return
+ * aux_help_alias - the help information for the builtin alias.
+ * Return: no return printed.
  */
 void aux_help_alias(void)
 {
@@ -27,8 +27,8 @@ void aux_help_alias(void)
 	write(STDOUT_FILENO, help, _strlen(help));
 }
 /**
- * aux_help_cd - Help information for the builtin alias.
- * Return: no return
+ * aux_help_cd - the help information for the builtin alias.
+ * Return: no return printed.
  */
 void aux_help_cd(void)
 {
